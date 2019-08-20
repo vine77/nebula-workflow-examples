7967e5a1dcc029f6f31b17db2b10237d606ed33f
Kenaz <kenaz@puppetlabs.com>
2019-08-19 22:31:16 -0700

    Adding GKE provisioning example

---
+++ provision-gke-cluster/infra/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_version = ">= 0.11.11"
+  backend "gcs" {
+    bucket = "kenazk-sandbox"
+    prefix = "kenazk"
+  }
+}
+
+provider "google" {
+  credentials = "${var.google-credentials}"
+  project     = "${local.workspace["gcp_project"]}"
+  region      = "${local.workspace["gcp_region"]}"
+}
+
+resource "google_container_cluster" "k8sexample" {
+  name               = "k8sexample-k8s-cluster"
+  description        = "example k8s cluster"
+  location           = "${local.workspace["gcp_location"]}"
+  initial_node_count = "${var.initial_node_count}"
+  enable_kubernetes_alpha = "true"
+  enable_legacy_abac = "true"
+
+  master_auth {
+    username = "${var.master_username}"
+    password = "${var.master_password}"
+
+    client_certificate_config {
+      issue_client_certificate = true
+    }
+  }
+
+  node_config {
+    machine_type = "${var.node_machine_type}"
+    disk_size_gb = "${var.node_disk_size}"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring"
+    ]
+  }
+}
---
+++ provision-gke-cluster/infra/variables.tf
@@ -0,0 +1,44 @@
+locals {
+  # Workaround for https://github.com/hashicorp/terraform/issues/15966
+  workspace-settings = {
+    file = "workspace.${terraform.workspace}.tfvars.json"
+
+    defaults = {
+      gcp_region                      = "us-east1"
+      gcp_location                    = "us-east1-b"
+      gcp_project                     = "kenazk-sandbox"
+    }
+  }
+
+  workspace = "${merge(local.workspace-settings.defaults, jsondecode(fileexists(local.workspace-settings.file) ? file(local.workspace-settings.file) : "{}"))}"
+}
+
+variable "google-credentials" {
+  description = "The Google credentials data to use for authentication"
+  default     = ""
+}
+
+variable "initial_node_count" {
+  description = "Number of worker VMs to initially create"
+  default = 1
+}
+
+variable "master_username" {
+  description = "Username for accessing the Kubernetes master endpoint"
+  default = "k8smaster"
+}
+
+variable "master_password" {
+  description = "Password for accessing the Kubernetes master endpoint"
+  default = "k8smasterk8smaster"
+}
+
+variable "node_machine_type" {
+  description = "GCE machine type"
+  default = "n1-standard-1"
+}
+
+variable "node_disk_size" {
+  description = "Node disk size in GB"
+  default = "20"
+}
